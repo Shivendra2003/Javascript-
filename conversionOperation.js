d7460223027be6f07fccede2e9635494f518425c
Shivendra Singh <87404911+Shivendra2003@users.noreply.github.com>
2024-09-04 06:26:13 +0000

    conversion of datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,18 @@
+let age= "18"
+console.log(typeof(age));  // String
+
+let valueInNumber= Number(age) // conversion
+
+console.log(typeof(valueInNumber)); // number
+console.log(valueInNumber);
+
+//NaN - not a number
+
+/*
+"33"- 33
+"33abc" - NaN
+true - 1
+false -0
+null - 0
+undefined - NaN
+*/
