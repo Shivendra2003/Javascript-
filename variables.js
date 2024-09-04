02305fcc9cbab73494357e5e10e9a77f815d405b
Shivendra Singh <87404911+Shivendra2003@users.noreply.github.com>
2024-09-04 05:55:16 +0000

    variables done

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountID= 1234   //fixed value- can not be changed 
+let accountEmail="xyz2gmail.com"
+var accountpassword ="123456"
+accountcity="Jaipur"
+
+accountEmail="abcgmail.com"
+accountpassword="9999"
+accountcity="lucknow"
+
+// console.log(accountID);
+console.table([accountID,accountEmail,accountpassword, accountcity])
+
+//In Javascript use only const and let. Avoid using var datatype.
