82ff78d7498399d1da0f055ae31c47f74babf4e1
Shivendra Singh <87404911+Shivendra2003@users.noreply.github.com>
2024-09-04 06:14:00 +0000

    datatypes done

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all the JS code as new version 
+
+//alert(3+3) // we are using node js not browser
+console.log(3+3);
+
+let name="Hitesh"
+let age=18
+let islogin = false
+
+//number
+//bignit
+//string
+//boolean
+//null - empty string
+//undefined - value is not assign
+//symbol - unique
+
+
+console.log(typeof(age)) // number
+console.log(typeof(null)) // object
